e5d17a3eef12a97df09ddfb7c8bfbbea188efc84
KabeloMaps <mpnkab004@myuct.ac.za>
2025-10-02 01:26:25 +0200

    Completed building the nav bar

--- home-landing-page/src/App.tsx
+++ home-landing-page/src/App.tsx
@@ -4,13 +4,13 @@ import "./App.css";
 import NavBar from "./components/NavBar.tsx";
 
 function App() {
-  return;
-
-  <>
-    <h1>
-      <NavBar />
-    </h1>
-  </>;
+  return (
+    <>
+      <h1>
+        <NavBar />
+      </h1>
+    </>
+  );
 }
 
 export default App;
--- home-landing-page/src/components/NavBar.tsx
+++ home-landing-page/src/components/NavBar.tsx
@@ -1,20 +1,94 @@
-import React from "react";
-import logo from "../images/logo.svg";
-
 export default function NavBar() {
   return (
     <div>
-      <section className="nav-bar">
-        <div className="logo-icon">
-          <img src={logo}></img>
-          <h1 className="tabs">Features</h1>
-          <h1 className="tabs">Company</h1>
-          <h1 className="tabs">Careers</h1>
-          <h1 className="tabs">About</h1>
-        </div>
-      </section>
+      <section className="header">
+        <nav className="topnav">
+          <img src="./public/images/logo.svg" alt="logo"></img>
+          <ul className="left-nav">
+            <li>
+              Features
+              <button type="button" className="btn-dropdown">
+                <img
+                  className="dropdown-arrow"
+                  src="./public/images/icon-arrow-down.svg"
+                  alt="arrow-icon"
+                />
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a href="#">
+                    <img
+                      src="./public/images/icon-todo.svg"
+                      alt="todo icon"
+                    ></img>
+                    Todo List
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="./publc/images/icon-calendar.svg"
+                      alt="calender icon"
+                    ></img>
+                    Calender
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src="./public/images/icon-reminders.svg"></img>
+                    Reminders
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="./public/images/icon-planning.svg"
+                      alt="planning icon"
+                    ></img>
+                    Planning
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              Company
+              <button type="button" className="btn-dropdown">
+                <img
+                  className="dropdown-arrow"
+                  src="./public/images/icon-arrow-down.svg"
+                  alt="arrow-icon"
+                ></img>
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a href="#">History</a>
+                </li>
+                <li>
+                  <a href="#">Our Team</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
 
-      <section className="hero"></section>
+          <ul className="right-nav">
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li className="register">
+              <a href="">Register</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </div>
   );
 }
